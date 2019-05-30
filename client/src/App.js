8fbdff3b715ad1b54827d6847baa91bbd1888e4a
Dylan Lederle-Ensign <dylan.lederle-ensign@viget.com>
2019-05-30 17:54:42 -0400

    Pull out Puppers component

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,15 @@
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import gql from "graphql-tag";
 import React from "react";
-import { ApolloProvider, Query } from "react-apollo";
+import { ApolloProvider } from "react-apollo";
 import "./App.css";
-
-const HTTP_URI = "http://localhost:4000";
+import { createClient } from "./util/apollo";
+import Puppers from "./Puppers";
 
 function App() {
-  const client = new ApolloClient({
-    link: new HttpLink({ uri: HTTP_URI }),
-    cache: new InMemoryCache()
-  });
-
-  const LIST_PUPPERS = gql`
-    {
-      listPuppers {
-        id
-        age
-        name
-        avatarUrl
-        favoriteToy
-        ownerName
-      }
-    }
-  `;
+  const client = createClient();
 
   return (
     <ApolloProvider client={client}>
-      <h1>Puppers!</h1>
-      <Query query={LIST_PUPPERS}>
-        {({ loading, error, data }) => {
-          if (loading) return "Loading...";
-          if (error) return `Error! ${error.message}`;
-
-          return (
-            <ul>
-              {data.listPuppers.map(pupper => (
-                <li>
-                  {pupper.name} belongs to {pupper.ownerName}, is {pupper.age}{" "}
-                  years old and loves to play with {pupper.favoriteToy}
-                </li>
-              ))}
-            </ul>
-          );
-        }}
-      </Query>
+      <Puppers />
     </ApolloProvider>
   );
 }
---
+++ client/src/Puppers.js
@@ -0,0 +1,42 @@
+import gql from "graphql-tag";
+import React from "react";
+import { Query } from "react-apollo";
+
+function Puppers() {
+  const LIST_PUPPERS = gql`
+    {
+      listPuppers {
+        id
+        age
+        name
+        avatarUrl
+        favoriteToy
+        ownerName
+      }
+    }
+  `;
+
+  return (
+    <div>
+      <h1>Puppers!</h1>
+      <Query query={LIST_PUPPERS}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+
+          return (
+            <ul>
+              {data.listPuppers.map(pupper => (
+                <li>
+                  {pupper.name} belongs to {pupper.ownerName}, is {pupper.age}{" "}
+                  years old and loves to play with {pupper.favoriteToy}
+                </li>
+              ))}
+            </ul>
+          );
+        }}
+      </Query>
+    </div>
+  );
+}
+export default Puppers;
