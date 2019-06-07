0aa12606b3dea442dc1626d85914c19964844561
Margaret Williford <margaretwilliford4@gmail.com>
2019-06-07 16:20:11 -0600

    Adding Woof Types

---
+++ lib/vigepets_web/schema/woof_types.ex
@@ -0,0 +1,50 @@
+defmodule Vigepets.Schema.WoofTypes do
+  use Absinthe.Schema.Notation
+  use Absinthe.Ecto, repo: Vigepets.Repo
+
+  alias Vigepets.Resolvers
+
+  @desc "A post (woof) on the site"
+  object :woof do
+    field :id, :id
+    field :body, :string
+
+    field :pupper, :pupper, resolve: assoc(:user)
+  end
+
+  object :woof_queries do
+    @desc "Get all woofs/posts"
+    field :woofs, list_of(:woof) do
+      resolve(&Resolvers.WoofResolver.list/3)
+    end
+
+    @desc "Get a specific woof"
+    field :woof, :woof do
+      arg(:id, non_null(:id))
+      resolve(&Resolvers.WoofResolver.show/3)
+    end
+  end
+
+  object :woof_mutations do
+    @desc "Create woof"
+    field :create_post, :post do
+      arg(:body, non_null(:string))
+
+      resolve(&Resolvers.WoofResolver.create/3)
+    end
+  end
+
+  object :woof_subscriptions do
+    field :woof_created, :woof do
+      config(fn _, _ ->
+        {:ok, topic: "woofs"}
+      end)
+
+      trigger(:create_woof,
+        topic: fn _ ->
+          "woofs"
+        end
+      )
+    end
+  end
+end
