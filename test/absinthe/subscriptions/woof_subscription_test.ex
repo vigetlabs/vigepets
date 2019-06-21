33ef4588c8b2b5256067a851a89f258a718f4c88
Margaret Williford <margaretwilliford4@gmail.com>
2019-06-21 14:11:07 -0600

    passing woof subscription test

--- test/absinthe/subscriptions/woof_subscription_test.ex
+++ test/absinthe/subscriptions/woof_subscription_test.ex
@@ -1,8 +1,11 @@
 defmodule VigepetsWeb.WoofSubscriptionsTest do
     use VigepetsWeb.SubscriptionCase
-  
+
+
     describe "Woof subscription" do
       it "updates on new woof", %{socket: socket} do
+
+        pupper = insert(:pupper)
         # Query to establish the subscription.
         subscription_query = """
           subscription {
@@ -12,35 +15,40 @@ defmodule VigepetsWeb.WoofSubscriptionsTest do
             }
           }
         """
-  
+
         # Push the query onto the socket.
         ref = push_doc(socket, subscription_query)
-  
+
         # Assert that the subscription was successfully created.
         assert_reply(ref, :ok, %{subscriptionId: _subscription_id})
-  
+
         # Query to create a new woof to invoke the subscription.
         create_woof_mutation = """
           mutation CreateWoof {
-            createWoof(body: "Big discussion") {
+            createWoof(body: "Big discussion", pupperId: #{pupper.id}) {
               id
               body
+              pupper {
+                id
+              }
             }
           }
         """
-  
+
         # Push the mutation onto the socket.
         ref =
           push_doc(
             socket,
             create_woof_mutation
           )
-  
+
         # Assert that the mutation successfully created the woof.
         assert_reply(ref, :ok, reply)
         data = reply.data["createWoof"]
         assert data["body"] == "Big discussion"
-  
+
+        assert data["pupper"]["id"] == to_string(pupper.id)
+
         # Assert that the subscription notified us of the new woof.
         assert_push("subscription:data", push)
         data = push.result.data["woofCreated"]
--- test/support/subscription_case.ex
+++ test/support/subscription_case.ex
@@ -1,27 +1,27 @@
 defmodule VigepetsWeb.SubscriptionCase do
     use ExUnit.CaseTemplate
-    
+
     using do
       quote do
         use VigepetsWeb.ChannelCase
         use Absinthe.Phoenix.SubscriptionTest, schema: VigepetsWeb.Schema
-        # use ExSpec
-        # import Vigepets.Factory
-  
+        use ExSpec
+        import Vigepets.Factory
+
         setup do
         #   pupper = insert(:pupper)
-  
+
           # When connecting to a socket, if you pass a token we will set the context's `current_user`
         #   params = %{
         #     "token" => sign_auth_token(user)
         #   }
-  
-          {:ok, socket} = Phoenix.ChannelTest.connect(VigepetsWeb.AbsintheSocket)#, params)
+
+          {:ok, socket} = Phoenix.ChannelTest.connect(VigepetsWeb.AbsintheSocket, nil)#, params)
           {:ok, socket} = Absinthe.Phoenix.SubscriptionTest.join_absinthe(socket)
-  
+
           {:ok, socket: socket}#, user: user}
         end
-  
+
         # defp sign_auth_token(user) do
         #   {:ok, token, _claims} = Guardian.encode_and_sign(user)
         #   token
