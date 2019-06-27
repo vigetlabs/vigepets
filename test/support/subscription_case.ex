678d9d65066e725515cecf4a598b3534db209246
Margaret Williford <margaretwilliford4@gmail.com>
2019-06-27 14:13:20 -0600

    remove commented code

--- test/support/subscription_case.ex
+++ test/support/subscription_case.ex
@@ -9,23 +9,12 @@ defmodule VigepetsWeb.SubscriptionCase do
         import Vigepets.Factory
 
         setup do
-        #   pupper = insert(:pupper)
-
-          # When connecting to a socket, if you pass a token we will set the context's `current_user`
-        #   params = %{
-        #     "token" => sign_auth_token(user)
-        #   }
-
           {:ok, socket} = Phoenix.ChannelTest.connect(VigepetsWeb.AbsintheSocket, nil)#, params)
           {:ok, socket} = Absinthe.Phoenix.SubscriptionTest.join_absinthe(socket)
 
           {:ok, socket: socket}#, user: user}
         end
-
-        # defp sign_auth_token(user) do
-        #   {:ok, token, _claims} = Guardian.encode_and_sign(user)
-        #   token
-        # end
+        
       end
     end
   end
